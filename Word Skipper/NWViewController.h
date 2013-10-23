a374a73bec7165339e728037e90b7adcc61e71c3
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-23 12:29:31 -0700

    Initial Commit

---
+++ Word Skipper/NWViewController.h
@@ -0,0 +1,13 @@
+//
+//  NWViewController.h
+//  Word Skipper
+//
+//  Created by Nicholas Wargnier on 10/23/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWViewController : UIViewController
+
+@end
---
+++ Word SkipperTests/Word_SkipperTests.m
@@ -0,0 +1,34 @@
+//
+//  Word_SkipperTests.m
+//  Word SkipperTests
+//
+//  Created by Nicholas Wargnier on 10/23/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Word_SkipperTests : XCTestCase
+
+@end
+
+@implementation Word_SkipperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
